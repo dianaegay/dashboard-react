6608486f89509197b63bd077695bd63720b6c27f
dianaegay <dianaegay29@gmail.com>
2022-02-26 13:31:44 -0300

    agregando estructura y componentes

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from './Main';
+function App() {
+  return (
+    <React.Fragment>
+      <div id="wrapper">
+        <Main /> 
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
+
---
+++ src/components/Movies.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Movies() {
+    return (
+    <div>
+        <h1> Aqui van tablas</h1>
+        <p> ES OPCIONAL con id, titulo y genero: nombre</p>
+    </div>
+    );
+  }
+
+export default Movies;
